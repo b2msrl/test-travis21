a1476543820a98b71aa99c3465eb979dd6bae287
alevento <a@a.it>
2016-10-20 19:27:40 +0200

    pippo

---
+++ src/prova.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alessandro
+ * Date: 20/10/2016
+ * Time: 19:06.
+ */
+
+namespace B2msrl\src;
+
+class prova
+{
+    public function dummy()
+    {
+        echo 'pippo';
+    }
+}
