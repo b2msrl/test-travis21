3732776c738141cd200e63a99c81ad6813e9f661
alevento <a@a.it>
2016-10-26 11:28:48 +0200

    test vaddy

---
+++ src/provaTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alessandro
+ * Date: 26/10/2016
+ * Time: 11:27
+ */
+
+namespace B2msrl\src;
+
+
+class provaTest extends \Padosoft\LaravelTest\TestBase
+{
+
+    public function setUp()
+    {
+        //$this->workbench = new Workbench();
+        parent::setUp();
+    }
+    /**test
+     *
+     */
+    public function testDummy() {
+        $this->assertTrue(true);
+    }
+}
